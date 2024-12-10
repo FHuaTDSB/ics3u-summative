b424497a942337d284afe941bf1069eea213f266
Felix Hua <felix.hua@student.tdsb.on.ca>
2024-12-10 13:54:09 -0500

    Initialised Firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,6 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ-x18GBB_CXYXwccmth_mXfG-bOvKIiM",
+    authDomain: "summative-45a0e.firebaseapp.com",
+    projectId: "summative-45a0e",
+    storageBucket: "summative-45a0e.firebasestorage.app",
+    messagingSenderId: "45120500861",
+    appId: "1:45120500861:web:4972832fc9121ef25e6d14"
+};
 
 
 const config = initializeApp(firebaseConfig)
